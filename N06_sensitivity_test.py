0866f69e2f45a1c1c2e26bd32b357b2f3acc852f
Susan74486 <122844335+Susan74486@users.noreply.github.com>
2023-08-03 21:08:59 +0800

    Add files via upload
    英文版

--- N06_sensitivity_test.py
+++ N06_sensitivity_test.py
@@ -1,9 +1,6 @@
-###比较按行归一化和按列归一化结果的不同
-# f1=open('Filename_potential_duplicate_ALL_1995_unique_1119.txt','r')
-# f2=open('Filename_potential_duplicate_ALL_1995_unique_1119_new.txt','r')
-
-f1=open('Filename_potential_duplicate_ALL_2011_unique_1119.txt','r')
-f2=open('Filename_potential_duplicate_ALL_2011_unique_1119 _new.txt','r')
+### This program is used for sensitivity test, and is used to compare the difference before and after the modified program
+f1=open('Filename_potential_duplicate_ALL_2011_unique_1119_new.txt','r')
+f2=open('Filename_potential_duplicate_ALL_2011_unique_1119_new2.txt','r')
 
 txt1=f1.read()
 txt2=f2.read()
@@ -14,10 +11,13 @@ f2.close()
 line1=txt1.split()
 line2=txt2.split()
 
-outfile=open('different_f1f2.txt','w')
+#outfile=open('sensitivity_test1975.txt','w')
+#outfile=open('sensitivity_test1995.txt','w')
+outfile=open('sensitivity_test2011.txt','w')
+
 
 for i in line1:
-    #查看1中文件是否在2中存在
+    # Check whether the file in file1 exists in file2
     if i not in line2:
         outfile.write(i)
         outfile.write('\n')
@@ -25,14 +25,14 @@ outfile.write('Above content in file1,but not in file2')
 outfile.write('\n')
 outfile.write('\n')
 for j in line2:
-    #查看1中文件是否在2中存在
+    # Check whether the file in file2 exists in file1
     if j not in line1:
         outfile.write(j)
         outfile.write('\n')
 outfile.write('Above content in file2,but not in file1')
 outfile.write('\n')
 
-print('核对结束！')
+print('Finish!')
 
 
 
