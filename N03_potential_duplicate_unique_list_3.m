3d0ca0bf5cec1c48e1c110693034c9fb05d3158b
Susan74486 <122844335+Susan74486@users.noreply.github.com>
2023-08-04 17:04:17 +0800

    Add files via upload

--- N03_potential_duplicate_unique_list_3.m
+++ N03_potential_duplicate_unique_list_3.m
@@ -1,10 +1,9 @@
-%%%%%%%% duplicate list 去重取唯一
-%%%%%%%% N02步骤中尝试搜索了很多个潜在的重复对文件，现在需要把这些文件里面的文件对进行合并（多个txt文件合并成一个大的txt文件）
-%%%% 以便在下一步duplicate 检查中进行处理；但是合并的时候可能会有重复的文件对，这个时候需要“去重”，保证文件名的唯一性
+%%% In the N02 step, we  search for many potential duplicate files, now we need to merge the file pairs
+%%% Merge multiple txt files into one large txt file to ensure the uniqueness of the file name
 clear
 clc
 
-files_txt=dir('./potential_duplicates_output/2008/potential_dup*.txt');
+files_txt=dir('./potential_duplicates_output/1975/potential_dup*.txt');
 
 filenames_column1={};
 filenames_column2={};
@@ -16,15 +15,15 @@ for i=1:length(files_txt)
     fid=fopen(file1,'r');
     
     while ~feof(fid)
-        str=fgetl(fid);
-        str=strtrim(str);
+        str=fgetl(fid); % read a line in a file
+        str=strtrim(str); % crop the Spaces at the beginning and end of the string
         s=regexp(str,'\s+','split');
         for k=2:length(s)
             filename1=s{1};
             filename2=s{k};
             
-            filename_combine=[filename1,' ',filename2];
-            
+            filename_combine=[filename1,' ',filename2]; 
+                                                        
             filenames_column1{m}=filename1;
             filenames_column2{m}=filename2;
             filenames_combines{m}=filename_combine;
@@ -33,13 +32,13 @@ for i=1:length(files_txt)
     end
 end
 
-%%%%去重
+%%% Ensure the uniqueness of the file name
 filename_unique_pairs=unique(filenames_combines);
 
 
-%%%%%% 重新输出新的potential_list
-fid=fopen('./potential_duplicate_ALL_2008_unique_1119.txt','w+');
-%%%%%输出原始数据文件
+%%% Reoutput potential_list
+fid=fopen('./potential_duplicate_ALL_1992_unique_1119.txt','w+');
+%%% Output filename
 for m=1:length(filename_unique_pairs)
     s=regexp(filename_unique_pairs{m},'\s+','split');
     output_filename1=s{1};
