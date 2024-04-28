e3c7719e71f70d2de6b0b270d1f8e6a06acdc696
Zhetao Tan <tanzhetao19@mails.ucas.ac.cn>
2024-04-28 12:14:16 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-name='DC_OCEAN'
-__author__ = 'Zhetao Tan (IAP/CAS), Xinyi Song (IAP/CAS), Lijing Cheng (IAP/CAS)'
-__author__ = 'IQuOD'
-__email__ = 'tanzhetao@mail.iap.ac.cn; songxinyi231@mails.ucas.ac.cn' 
-__version__ = '1.1'
