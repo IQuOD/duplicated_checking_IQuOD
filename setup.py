e17b23a8c0ddb38ae11008e6e788775ffec0246b
Guilherme Castelão <guilherme@castelao.net>
2024-07-10 05:04:59 -0600

    Migrating to pyproject.toml (#9)
    * Initializing pyproject.toml based on previous setup.py
    
    * Defining environment with pixi
    
    * fix: argparse is a standard library
    
    * Updating .gitignore and .gitattributes
    
    * Adding pixi.lock
    
    * Bumping to at least Python 3.8
    
    * Reference to license file
    
    * clean: PKG-INFO is auto generated whenever the package is built
    
    * cfg: target version py38
    
    * cfg: Moving data files inventory to pyproject.toml
    
    * Initializing unit test area
    
    * Updating pixi.lock
    
    * cfg: Ignoring __pycache__

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 import setuptools
 
-with open("README.md",'r',encoding='utf-8') as fh:
-    long_description=fh.read()
-
-
 setuptools.setup(
-    name="DC_OCEAN",
-    version="1.3.1",
     author ="Zhetao Tan; Xinyi Song; Lijing Cheng; Rebecca Cowley, Huifeng Yuan, Guilherme Castelao, Simona Simoncelli, Shoichi Kizu, Ricardo Locarnini, Tim Boyer, Franco Reseghetti, Viktor Gouretski",
-    author_email = "tanzhetao@mail.iap.ac.cn; songxinyi231@mails.ucas.ac.cn",
-    description = "DC_OCEAN: An algorithm to detect the ocean in-situ duplicate profiles (Song et al., 2024, FMS)",
-    long_description = long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/IQuOD/duplicated_checking_IQuOD/",
-    include_package_data=True,
-    package_data={'DC_OCEAN':['util/*','support/*.py','tests/Examples_netCDF_files/*','Input_files/WOD18_sample_1995/*','Input_files/*.txt','tests/*.py']},
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=['numpy >= 1.19.1','netCDF4 >= 1.5.4','timezonefinder >=6.0.1','pandas >=1.0.3','scipy >=1.7.3','argparse >=1.4.0'],
-    python_requires='>=3.8',
 )
---
+++ tests/test_DC_OCEAN.py
@@ -0,0 +1,3 @@
+"""Tests for DC_OCEAN package."""
+
+from DC_OCEAN.util import country_table, compair_main
